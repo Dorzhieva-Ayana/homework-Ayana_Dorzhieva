4dcdbf2a383414d657f4ff8e8ef7cb6e16765ab6
Dorzhieva-Ayana <133004938+Dorzhieva-Ayana@users.noreply.github.com>
2023-05-09 16:47:55 +0300

    created index.html + script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const name = "Аяна";
+const surname = "Доржиева";
+const YearOfBirth = 1997;
+const profession = "юрист";
+const city = "Москва";
+const city2 = "Саратов";
+const year1 = 2014;
+const city3 = "Иркутск";
+const year2 = 2018;
+const yearMov = 2020;
+const hobby = "волейбол";
+const h2 = "кататься на велосипеде";
+const musicalGroup = "Imagine Dragons";
+const target = "плавать";
+let year = 2023;
+
+console.log(`Привет! Меня зовут ${name} ${surname}. Мне ${year - YearOfBirth} лет.
+${year - year1} лет назад после окончания школы я переехала в ${city2}, а после для дальнейшей учебы через ${year2-year1} года я переехала в город ${city3}.
+С октября ${yearMov} года я живу и работаю ${profession}ом в столице. 
+Уже много лет я увлекаюсь ${hobby}ом. Это мой любимый вид спорта.
+В свободное от работы время мне нравится ${h2} под свою любимую музыку в лице ${musicalGroup}. В ближайшем будущем я хочу научиться хорошо ${target}.`);
