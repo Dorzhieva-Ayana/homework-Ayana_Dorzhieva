397ddf92abea54978975c4a9e9a5df75c6bfd65c
Dorzhieva-Ayana <133004938+Dorzhieva-Ayana@users.noreply.github.com>
2023-05-13 19:13:52 +0300

    added finish changes homework 6

--- Js 6 object array/script.js
+++ Js 6 object array/script.js
@@ -1,47 +1,78 @@
-
+ 
 let Pizza = {
-        name: `Пицца с грушей и горгонзолой`,
-        ingredients: `тесто, сыр Горгонзола, груша`, 
-        price: 1000,
-        size: `35 cm`       
+        ingredient1: `тесто`,
+        price1: 100,
+        ingredient2: `груша`,
+        price2: 200,
+        ingredient3: `сыр`,
+        price3: 600,
+        ingredient4: `грецкий орех`,
+        price4: 90
     };
 let Roll = {
-        name: `рол Филадельфия`,
-        ingredients: `нори, сыр Филадальфия, рис, огурец, лосось`, 
-        price: 750,
-        weight: `500 grams`
+        ingredient1: `нори`,
+        price1: 10,         
+        ingredient2: `сыр`, 
+        price2: 200, 
+        ingredient3: `рис`, 
+        price3: 150,
+        ingredient4: `огурец`, 
+        price4: 30,
+        ingredient5: `лосось`, 
+        price5: 400
     };
-
 let Buuza = {
-        name: `Буузы`,
-        ingredients: `тесто, домашний фарш`, 
-        price: 90,
-        weight: `70 grams`
+        ingredient1: `тесто`, 
+        price1: 10, 
+        ingredient2: `фарш домашний`, 
+        price2: 30,
     };
 
-const food = [Pizza, Roll, Buuza];
+let food = [Pizza, Roll, Buuza];
 alert(food.length);
 console.log (food);
 
 //3.удаляем одно блюдо
-food.pop ();
-console.log (food[2]);
-
-//4.добавляем это блюдо на 1 место, удаляем одно его свойство
-food.unshift (Buuza);
-delete Buuza.price;
+food.pop();
 console.log (food);
 
-//5.Затраты на все 3 блюда.Цена за все 3 блюда.
-console.log (1000+750+90); //1840
-console.log (1840 / 70 * 100); //2628,57..
-let mprice = Math.floor (2628,57)
-console.log (mprice);
+//4.добавляем это блюдо на 1 место, удаляем один ингредиент
+food.unshift (Buuza);
+delete Buuza.ingredient1;
+console.log (Buuza.ingredient1);
+console.log (food);
 
-//6.Прибыль с 3 блюд
-console.log (2628 - 1840);
+//5.Затраты
+let costPizza = (Pizza.price1) + (Pizza.price2) + (Pizza.price4) + (Pizza.price4);
+console.log (costPizza);
 
+let costRoll = (Roll.price1) + (Roll.price2) + (Roll.price4) + (Roll.price4);
+console.log (costRoll);
 
-//7.Удаляем из массива все элементы, кроме одного
+let costBuuza = (Buuza.price1) + (Buuza.price2);
+console.log (costBuuza);
+
+//5.Цена в меню
+let pricePizza = (costPizza) + (costPizza) * 70 / 100
+console.log (pricePizza);
+
+let priceRoll = (costRoll) + (costRoll) * 70 / 100
+console.log (priceRoll);
+
+let priceBuuza = (costBuuza) + (costBuuza) * 70 / 100
+console.log (priceBuuza);
+
+//6.Прибыль
+let cashPizza = (pricePizza) - (costPizza);
+console.log (cashPizza);
+
+let cashRoll = (priceRoll) - (costRoll);
+console.log (cashRoll);
+
+let cashBuuza = (priceBuuza) - (costBuuza);
+console.log (cashBuuza);
+
+//7.Удаляем из массива все элементы кроме одного
 food.splice (0,2);
 console.log (food);
+
